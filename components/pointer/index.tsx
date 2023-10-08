8d61d93a68e999fc6ecdb75dd9e1ae58445e6a58
Brayan Bernal <brayanbernal0710@gmail.com>
2023-10-07 21:33:22 -0400

    Adding some responsive improvements

--- components/pointer/index.tsx
+++ components/pointer/index.tsx
@@ -1,10 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { gsap } from "gsap";
 
 export default function Pointer() {
+  const [showPointer, setShowPointer] = useState(true);
+  const dispatchPointer = () => {
+    if (window.innerWidth < 961) {
+      setShowPointer(false);
+      return;
+    }
+    setShowPointer(true);
+    return;
+  }
   useEffect(()=>{
     const pointer = document.querySelector('#Pointer');
+    window.addEventListener('load', dispatchPointer);
+    window.addEventListener('resize', dispatchPointer);
     window.addEventListener('mousemove',(event)=>{
+      if (window.innerWidth < 961) return;
       const { clientX, clientY } = event;
       const x = Math.round((clientX / window.innerWidth) * 100);
       const y = Math.round((clientY / window.innerHeight) * 100);
@@ -18,12 +30,16 @@ export default function Pointer() {
     });
   },[]);
   return <>
-    <span id="Pointer" className="white transparent">
-      <span className="white medium">
-        <span className="white strong">
-          <span className="white full"></span>
+   {
+    showPointer && (
+      <span id="Pointer" className="white transparent">
+        <span className="white medium">
+          <span className="white strong">
+            <span className="white full"></span>
+          </span>
         </span>
       </span>
-    </span>
+    )
+   }
   </>;
 }
--- components/project_card/index.tsx
+++ components/project_card/index.tsx
@@ -1,15 +1,38 @@
 import Project from "@/models/projects_model";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function ProjectCard(project: Project) {
+  const [imageSize, setImageSize] = useState({
+    height: 100,
+    width: 200
+  });
   const techsList = project.techs?.map((tech, index)=> {
     return <li key={index} className="project-tech">{tech}</li>;
   }) || [];
+  const resolveImage = () => {
+    if (window.innerWidth < 426) {
+      setImageSize({
+        width:410,
+        height:205
+      });
+      return;
+    }
+    setImageSize({
+      width: 200, 
+      height:100
+    });
+    return;
+  }
+  useEffect(()=>{
+    window.addEventListener('load',resolveImage);
+    window.addEventListener('resize',resolveImage);
+  },[]);
   return <>
     <li key={project.name} className="project-card">
       <a href={project.link_url} className="project-link">
         <div className="project-image">
-          <Image src={project.image_url} alt={project.name} width={200} height={100}/>
+          <Image src={project.image_url} alt={project.name} width={imageSize.width} height={imageSize.height}/>
         </div>
         <div className="project-body">
           <h3 className="project-name">{project.name}</h3>
