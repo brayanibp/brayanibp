9d12d044ae90be76592842fee35d2cfd902d8db4
Brayan Bernal <brayanbernal0710@gmail.com>
2024-06-02 18:28:14 -0400

    Generating dynamic metadata on post pages

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -2,6 +2,31 @@ import styles from "./page.module.css";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import Image from "next/image";
+import { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: {
+    post: string,
+  }
+}
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  const { post } = params;
+  const { frontmatter } = await fetchPost(post);
+  const previousData = await parent;
+  const previousImages = previousData?.openGraph?.images || [];
+  const previousKeywords = previousData?.keywords || [];
+  return {
+    title: frontmatter.title,
+    description: frontmatter.description,
+    openGraph: {
+      title: frontmatter.title,
+      description: frontmatter.description,
+      images: [frontmatter.thumbnailUrl, ...previousImages],
+    },
+    keywords: [...(frontmatter.tags || []), ...previousKeywords]
+  };
+}
 
 const fetchPost = async (post: string) => {
   const fs = require("fs");
