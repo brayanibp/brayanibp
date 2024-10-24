80e9d73e351a4ad2ef5d026a75a360be8383aaae
Brayan Bernal <brayanbernal0710@gmail.com>
2024-10-24 09:20:40 -0400

    implement dynamic import to improve home page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
-import Social from "@/components/Social";
+import dynamic from "next/dynamic";
+
+const About = dynamic(import("@/components/About"));
+const Contact = dynamic(import("@/components/Contact"));
+const Experience = dynamic(import("@/components/Experience"));
+const Projects = dynamic(import("@/components/Projects"));
+const Social = dynamic(import("@/components/Social"));
 
 const Home = () => {
   return (
