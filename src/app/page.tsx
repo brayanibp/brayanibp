d802a445cfdc1c8cefb856da2b82d02843f59fc3
Brayan Bernal <brayanbernal0710@gmail.com>
2024-10-24 09:49:53 -0400

    fix for dynamic imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+"use server";
 import dynamic from "next/dynamic";
 
-const About = dynamic(import("@/components/About"));
-const Contact = dynamic(import("@/components/Contact"));
-const Experience = dynamic(import("@/components/Experience"));
-const Projects = dynamic(import("@/components/Projects"));
-const Social = dynamic(import("@/components/Social"));
+const About = dynamic(() => import("@/components/About").then((res)=>res));
+const Contact = dynamic(() => import("@/components/Contact"));
+const Experience = dynamic(() => import("@/components/Experience"));
+const Projects = dynamic(() => import("@/components/Projects"));
+const Social = dynamic(() => import("@/components/Social"));
 
 const Home = () => {
   return (
