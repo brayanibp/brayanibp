e71283c0b587d06c57e09dec1e0f71aaee4ccbda
Brayan Bernal <brayanbernal0710@gmail.com>
2024-10-24 22:10:56 -0400

    Adding fallback if inter font import fails, also fixing content tables styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+// import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], adjustFontFallback: true, fallback: ["Arial", "sans-serif"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://brayanibp.dev"),
